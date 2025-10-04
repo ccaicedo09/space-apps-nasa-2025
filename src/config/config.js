efab922e6f55cf0fe33175c2b3a9a5a45cffd0cf
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-10-04 10:47:09 -0500

    deleted .env

--- src/config/config.js
+++ src/config/config.js
@@ -6,13 +6,9 @@ const {
   USER_DB,
   PASS_DB,
   SERVER_DB,
-  SALT_ROUNDS,
-  GOOGLE_APP_PASS
 } = process.env
 
 export default {
   port: PORT || 3000,
   databaseUrl: `mongodb+srv://${USER_DB}:${PASS_DB}@${SERVER_DB}.ugygvvt.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSpaceApps2025`,
-  salt: Number(SALT_ROUNDS),
-  googleAppPass: GOOGLE_APP_PASS
 }
